42d93dcb2ba6b942cfeedd59f223d5a4545f9556
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2019-12-31 22:36:53 +0530

    Add files via upload

---
+++ resume.py
@@ -0,0 +1,54 @@
+x='''
+                                   RESUME
+
+
+
+    Aastha Harinkhere
+    Ward no. 25 Mardikar gali,
+    Balaghat,
+    Madhya Pradesh, 481001
+    6265275727
+    aasthaharinkhere272000@gmail.com
+____________________________________________________________________________________________________________
+
+  PERSONAL DETAILS
+    Father Name      : Rakesh Harinkhere
+    Mother Name      : Ratna Harinkhere
+    Sex              : Female
+    Date of Birth    : 02/07/2000
+____________________________________________________________________________________________________________
+  
+  ACADEMIC DETAILS
+    |S.no.|Academic year |         Degree         |        Institute       |Percentage/CGPA|
+    |     |              | BE in Computer science | Barkatullah University |               |   
+    |  1  | 2018-Present |   and Engineering      |      Institute of      |     N/A       |
+    |     |              |                        |    technology, Bhopal  |               |
+    |  2  |   2017-18    |       Class 12         |    Kendra Vidyalaya,   |     77.8%     |
+    |     |              |                        |         Balaghat       |               |
+    |     |              |                        |     Holy Home English  |               |
+    |  3  |  2015-16     |       Class 10         |        High School     |     93        |
+    |     |              |                        |          Balaghat      |               |
+____________________________________________________________________________________________________________
+
+  SKILLS
+    •) C,C++,Pythom
+    •) HTML,CSS,PHP
+    •) MySQL,Oracle
+    •) Embedded C
+____________________________________________________________________________________________________________
+
+  LANGUAGES
+    •) English
+    •) Hindi
+____________________________________________________________________________________________________________
+
+  OTHERS
+    Github Username: Aastha02-7
+    LinkedIn Id.   : aasthaharinkhere272000@gmail.com
+    Hackerank Id.  : aasthaharinkhere272000@gmail.com
+    Canva Id.      : aasthaharinkhere272000@gmail.com
+____________________________________________________________________________________________________________
+'''
+f= open("Aashu.txt","w+")
+f.write(x)
+f.close()
